550facebb8ba7b030faa08bae3feabd00bdc9e4a
lobarzanek <lukiob@gmail.com>
2024-01-06 16:35:05 +0100

    Prepare view models for navigation

---
+++ ViewModels/TeamsVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFDeskManager.Utilities.Base;
+
+namespace WPFDeskManager.ViewModels
+{
+    public class TeamsVM : ViewModelBase
+    {
+    }
+}
